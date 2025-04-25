b5adc3786ee616157a3cd611f816602d450765a6
yoav <yoav@fedora>
2025-04-25 23:11:45 +0300

    Tests now include auto adding directories
    Change-Id: I6252e324a65a4e41e259745c1e0fcd5f91164457

--- src/test/java/fairy/locals/BasicTests.java
+++ src/test/java/fairy/locals/BasicTests.java
@@ -2,16 +2,17 @@ package fairy.locals;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.time.Duration;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
 import fairy.core.AItem;
 import fairy.core.AItemActioner;
 import fairy.core.Directory;
 
-class BasicTests {
-	@Test
-	void test() throws Throwable {
+public class BasicTests {
+  @Test
+  void test() throws Throwable {
 		try {
 			LocalFileSystem localFileSystem = LocalFileSystem.getLocalFileSystem();
 			localFileSystem.getItemsMonitor().addItemActioners(new AItemActioner[] {new AItemActioner() {
@@ -45,9 +46,14 @@ class BasicTests {
 					.getDirectory("yoav")
 					.getDirectory("3C9550D109262BB2")
 					.getDirectory("y");
-			localFileSystem.getItemsMonitor().markItemForAction(y.getDirectory("New1"));
-			localFileSystem.getItemsMonitor().markItemForAction(y.getDirectory("New2"));
-			localFileSystem.getItemsMonitor().startMonitor();
+			localFileSystem.getItemsMonitor().markItemForAction(y);
+			y.getPath().resolve("sandbox").toFile().mkdir();
+			Directory sandbox = y.getDirectory("sandbox");
+			sandbox.addDirectory("NF1").addDirectory("NF1.1");
+			sandbox.addDirectory("NF2").addDirectory("NF2.1");
+			
+//			localFileSystem.getItemsMonitor().markItemForAction(new1);
+//			localFileSystem.getItemsMonitor().markItemForAction(new2);
 			Thread.sleep(Long.MAX_VALUE);
 		} catch (IOException e) {
 			e.printStackTrace();
